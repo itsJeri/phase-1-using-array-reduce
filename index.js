18a4f6105001fe1653c12628f3245113a6729451
Jerry Tong <zihangtong@gmail.com>
2021-11-27 18:29:31 -0500

    commit tree f4b78b4e8df00afeef717812e05d9a38d7095367

--- index.js
+++ index.js
@@ -1,3 +1,9 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
+// sum of all in batteryBatches array
+// function batteryCounter(total, batteryBatch) {
+//     return total + batteryBatch;
+// }
+
+let totalBatteries = batteryBatches.reduce(function(total, batteryBatch) {return total + batteryBatch}, 0);
